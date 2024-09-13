ae7e95aace7ddf839fc874d4db8a524887397fdf
ChristianEMV <20193tn065@utez.edu.mx>
2024-09-12 18:54:17 -0600

    [UPDATE "Nueva actualizacion"]

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ self.addEventListener('fetch', (event)=>{
     if(event.request.url.includes('style.css')){
         const respuesta = new Response(
             `body{
-                color: blue;
+                color: white;
                 background-color: #000;
             }`,
             {
